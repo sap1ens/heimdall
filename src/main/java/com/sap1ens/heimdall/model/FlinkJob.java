1986f0b4226b53279234affa8b062477f3dc263e
Yaroslav Tkachenko <sapiensy@gmail.com>
2023-06-07 21:34:08 -0700

    Tweak model mapping

--- src/main/java/com/sap1ens/heimdall/model/FlinkJob.java
+++ src/main/java/com/sap1ens/heimdall/model/FlinkJob.java
@@ -6,19 +6,13 @@ import org.apache.flink.kubernetes.operator.api.FlinkDeployment;
 public record FlinkJob(
     String name, String status, Long startTime, String image, Integer parallelism) {
   public static FlinkJob from(FlinkDeployment flinkDeployment) {
-    var containerTemplate =
-        Optional.ofNullable(flinkDeployment.getSpec().getPodTemplate())
-            .map(podTemplate -> podTemplate.getSpec().getContainers().get(0));
-    var image = flinkDeployment.getSpec().getImage();
-
     return new FlinkJob(
         flinkDeployment.getMetadata().getName(),
         flinkDeployment.getStatus().getJobStatus().getState(),
         Optional.ofNullable(flinkDeployment.getStatus().getJobStatus().getStartTime())
             .map(Long::parseLong)
             .orElse(null),
-        image, // TODO
-        0 // TODO
-        );
+        flinkDeployment.getSpec().getImage(),
+        flinkDeployment.getSpec().getJob().getParallelism());
   }
 }
