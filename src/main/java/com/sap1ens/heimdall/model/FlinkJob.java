f8c485218a7f7e68d07db5f54e526644b4f14f8e
Yaroslav Tkachenko <sapiensy@gmail.com>
2023-06-09 11:27:13 -0700

    Refactor FlinkJob details

--- src/main/java/com/sap1ens/heimdall/model/FlinkJob.java
+++ src/main/java/com/sap1ens/heimdall/model/FlinkJob.java
@@ -1,18 +1,4 @@
 package com.sap1ens.heimdall.model;
 
-import java.util.Optional;
-import org.apache.flink.kubernetes.operator.api.FlinkDeployment;
-
 public record FlinkJob(
-    String name, String status, Long startTime, String image, Integer parallelism) {
-  public static FlinkJob from(FlinkDeployment flinkDeployment) {
-    return new FlinkJob(
-        flinkDeployment.getMetadata().getName(),
-        flinkDeployment.getStatus().getJobStatus().getState(),
-        Optional.ofNullable(flinkDeployment.getStatus().getJobStatus().getStartTime())
-            .map(Long::parseLong)
-            .orElse(null),
-        flinkDeployment.getSpec().getImage(),
-        flinkDeployment.getSpec().getJob().getParallelism());
-  }
-}
+    String name, String status, Long startTime, String shortImage, Integer parallelism) {}
--- src/main/java/com/sap1ens/heimdall/service/K8sOperatorFlinkJobLocator.java
+++ src/main/java/com/sap1ens/heimdall/service/K8sOperatorFlinkJobLocator.java
@@ -9,12 +9,16 @@ import io.fabric8.kubernetes.client.dsl.MixedOperation;
 import io.fabric8.kubernetes.client.dsl.Resource;
 import jakarta.enterprise.context.ApplicationScoped;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import org.apache.flink.kubernetes.operator.api.FlinkDeployment;
+import org.apache.flink.kubernetes.operator.api.spec.JobSpec;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class K8sOperatorFlinkJobLocator implements FlinkJobLocator {
+  private static final String TM_NUMBER_OF_TASK_SLOTS = "taskmanager.numberOfTaskSlots";
+
   @ConfigProperty(name = "heimdall.k8s.namespace-to-watch")
   String k8sNamespace;
 
@@ -29,6 +33,47 @@ public class K8sOperatorFlinkJobLocator implements FlinkJobLocator {
     var listOptions = new ListOptions();
     var flinkDeployments =
         flinkDeploymentK8Client.inNamespace(k8sNamespace).list(listOptions).getItems();
-    return flinkDeployments.stream().map(FlinkJob::from).collect(Collectors.toList());
+    return flinkDeployments.stream().map(this::toFlinkJob).collect(Collectors.toList());
+  }
+
+  private FlinkJob toFlinkJob(FlinkDeployment flinkDeployment) {
+    return new FlinkJob(
+        flinkDeployment.getMetadata().getName(),
+        flinkDeployment.getStatus().getJobStatus().getState(),
+        Optional.ofNullable(flinkDeployment.getStatus().getJobStatus().getStartTime())
+            .map(Long::parseLong)
+            .orElse(null),
+        getShortImage(flinkDeployment),
+        getParallelism(flinkDeployment));
+  }
+
+  private String getShortImage(FlinkDeployment flinkDeployment) {
+    var image = flinkDeployment.getSpec().getImage();
+    return image.contains("/") ? flinkDeployment.getSpec().getImage().split("/")[1] : image;
+  }
+
+  private int getParallelism(FlinkDeployment flinkDeployment) {
+    var parallelism = 0;
+    // First, try to get parallelism from the job spec
+    var jobSpecParallelism =
+        Optional.ofNullable(flinkDeployment.getSpec().getJob())
+            .map(JobSpec::getParallelism)
+            .orElse(0);
+    if (jobSpecParallelism != 0) {
+      parallelism = jobSpecParallelism;
+      // If parallelism is not set in the job spec, try to calculate it based on the number of
+      // replicas and configured task slots
+      // FIXME: this might not be accurate
+    } else {
+      var taskSlots =
+          Optional.ofNullable(flinkDeployment.getSpec().getFlinkConfiguration())
+              .map(config -> config.get(TM_NUMBER_OF_TASK_SLOTS))
+              .orElse(null);
+      if (taskSlots != null) {
+        var replicas = flinkDeployment.getSpec().getTaskManager().getReplicas();
+        parallelism = Integer.parseInt(taskSlots) * replicas;
+      }
+    }
+    return parallelism;
   }
 }
