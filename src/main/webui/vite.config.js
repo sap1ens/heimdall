06ca51ba2e7713288903cd46dd5d04ebf739e865
Yaroslav Tkachenko <sapiensy@gmail.com>
2023-06-09 15:32:35 -0700

    Init webui

---
+++ src/main/webui/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  server: {
+    port: 5173,
+    host: '127.0.0.1',
+    hmr: {
+      // for proxying from quarkus dev
+      port: 5173,
+      // for testing standalone
+      // protocol: 'ws',
+      host: '127.0.0.1',
+    }
+  }
+})
