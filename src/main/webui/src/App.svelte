94fdde360b27a931a4045873e2b919a522192a28
Yaroslav Tkachenko <sapiensy@gmail.com>
2023-06-23 13:37:52 -0700

    Change header + logo

--- src/main/webui/src/App.svelte
+++ src/main/webui/src/App.svelte
@@ -1,14 +1,14 @@
 <script>
+  import heimdallLogo from './assets/heimdall-logo.png'
   import FlinkJobs from './lib/FlinkJobs.svelte'
 </script>
 
 <main>
   <div class="w-11/12 mx-auto pb-3">
-    <h1 class="text-3xl font-bold py-6 text-[#e6516f]">🐿️ 👀 Heimdall: Flink Deployments UI</h1>
+    <div class="flex items-center justify-start">
+      <img src={heimdallLogo} alt="Heimdall Logo" width="100" height="100" />
+      <h1 class="text-3xl font-bold py-6 text-[#e6516f] ml-3">Heimdall</h1>
+    </div>
     <FlinkJobs/>
   </div>
 </main>
-
-<style>
-
-</style>
