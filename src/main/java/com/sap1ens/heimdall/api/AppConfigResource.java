8d350c0af5568166ba9d179a54ff27730c87d69b
Yaroslav Tkachenko <sapiensy@gmail.com>
2023-06-27 23:03:32 -0700

    Expose app version

--- src/main/java/com/sap1ens/heimdall/api/AppConfigResource.java
+++ src/main/java/com/sap1ens/heimdall/api/AppConfigResource.java
@@ -5,16 +5,21 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import java.util.Map;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/config")
 public class AppConfigResource {
 
   @Inject AppConfig appConfig;
 
+  @ConfigProperty(name = "quarkus.application.version")
+  String appVersion;
+
   @GET
   public Map<String, Object> index() {
     // Only cherry-picking certain properties, don't need to show the whole config
     return Map.of(
+        "appVersion", appVersion,
         "patterns", appConfig.patterns(),
         "endpointPathPatterns", appConfig.endpointPathPatterns());
   }
