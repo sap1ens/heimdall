27b91e1d7300e691da2e7f7fb2490520ee6519af
Yaroslav Tkachenko <sapiensy@gmail.com>
2023-07-03 11:00:37 -0700

    Change base

--- src/main/webui/vite.config.js
+++ src/main/webui/vite.config.js
@@ -4,6 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: './',
   server: {
     port: 5173,
     host: '127.0.0.1',
