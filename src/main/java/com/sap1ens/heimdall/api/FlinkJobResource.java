a282416c7ac8e297ca5b02aaa1c785a05865e53b
Yaroslav Tkachenko <sapiensy@gmail.com>
2024-04-14 11:33:32 -0700

    feat: use Blocking annotation

--- src/main/java/com/sap1ens/heimdall/api/FlinkJobResource.java
+++ src/main/java/com/sap1ens/heimdall/api/FlinkJobResource.java
@@ -2,12 +2,14 @@ package com.sap1ens.heimdall.api;
 
 import com.sap1ens.heimdall.model.FlinkJob;
 import com.sap1ens.heimdall.service.FlinkJobLocator;
+import io.smallrye.common.annotation.Blocking;
 import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import java.util.List;
 
+@Blocking
 @Path("/jobs")
 public class FlinkJobResource {
 
