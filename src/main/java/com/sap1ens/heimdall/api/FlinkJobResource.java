4004a7ec44b9ae40febfe82d838f935ff9874e83
Yaroslav Tkachenko <sapiensy@gmail.com>
2024-04-14 11:54:17 -0700

    feat: add API-level cache

--- src/main/java/com/sap1ens/heimdall/api/FlinkJobResource.java
+++ src/main/java/com/sap1ens/heimdall/api/FlinkJobResource.java
@@ -2,6 +2,7 @@ package com.sap1ens.heimdall.api;
 
 import com.sap1ens.heimdall.model.FlinkJob;
 import com.sap1ens.heimdall.service.FlinkJobLocator;
+import io.quarkus.cache.CacheResult;
 import io.smallrye.common.annotation.Blocking;
 import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
@@ -15,6 +16,7 @@ public class FlinkJobResource {
 
   @Inject Instance<FlinkJobLocator> flinkJobLocator;
 
+  @CacheResult(cacheName = "flink-jobs")
   @GET
   public List<FlinkJob> list() {
     return flinkJobLocator.get().findAll();
