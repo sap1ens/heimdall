53832f74fd75c8ee6d70ae979e8f02e1cb2d0ecd
govejero <govejero@nextdigital.es>
2025-11-05 16:53:13 +0100

    fix: warning

--- src/main/webui/src/lib/Modal.svelte
+++ src/main/webui/src/lib/Modal.svelte
@@ -16,7 +16,7 @@
         on:keydown={(e) => e.key === 'Escape' && dialog.close()}
         class="w-[500px] min-h-[200px] p-[25px] outline-none rounded shadow-lg border border-gray-300"
 >
-    <div role="document" on:click|stopPropagation on:keydown|stopPropagation>
+    <div role="presentation" on:click|stopPropagation on:keydown|stopPropagation>
         <slot />
         <div class="absolute top-2 right-2">
             <button type="button" title="Close" on:click={() => dialog.close()} class="inline-block cursor-pointer bg-transparent border-0 p-0 m-0">
